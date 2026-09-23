cabcd364c11d229c4d5f82a3e3a96a5e0888d472
Pankaj Kumar <163027635+Impactfulcoder@users.noreply.github.com>
2026-09-23 22:34:15 +0530

    Update app.run() to use default host
    Removed host specification from app.run()

--- backend/app.py
+++ backend/app.py
@@ -62,5 +62,6 @@ initialize_model()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(debug=False)
+    # app.run(host="0.0.0.0", port=5000, debug=False)
     # app.run(host="127.0.0.1",port=5000,debug=True)
